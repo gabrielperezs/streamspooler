61bd1181d73c9ff1d025a365911f5ac7b47cbded
Bernat Cabezas <bcabezas@apsl.net>
2025-03-20 12:29:03 +0100

    Fix compress with snappy

--- internal/compress/compress.go
+++ internal/compress/compress.go
@@ -1,25 +1,10 @@
-// from github.com/gallir/smart-relayer/lib/compress/compress.go
 package compress
 
 import (
 	"github.com/golang/snappy"
 )
 
-const (
-	MinCompressSize  = 256
-	CompressPageSize = 256
-)
-
-var (
-	magicSnappy = []byte("$sy$")
-)
-
 // Bytes compress []bytes
 func Bytes(b []byte) []byte {
-	n := snappy.MaxEncodedLen(len(b)) + len(magicSnappy)
-	// Create the required slice once
-	buf := make([]byte, (n/CompressPageSize+1)*CompressPageSize)
-	copy(buf, magicSnappy)
-	c := snappy.Encode(buf[len(magicSnappy):], b)
-	return buf[:len(c)+len(magicSnappy)]
+	return snappy.Encode(nil, b)
 }
